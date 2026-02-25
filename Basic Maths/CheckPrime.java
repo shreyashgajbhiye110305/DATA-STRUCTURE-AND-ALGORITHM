00ca616da7036f188cbd23e7f5c7675abe8406b3
SHREYASH GAJBHIYE <shreyash.gajbhiye110305@gmail.com>
2026-02-25 09:17:56 +0530

    Check Prime number.
    Question of checking prime number is done in optimized way as well.

---
+++ Basic Maths/CheckPrime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class CheckPrime{
+
+	public static String checkPrime(int n){
+		for(int i = 2; i < n; i++){
+			if(n%i == 0){
+				return "Not Prime";
+			}
+		}
+
+		return "Prime number";
+	}
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number:");
+		int number = sc.nextInt();
+		System.out.println(checkPrime(number));
+		
+	}	
+
+
+
+}
